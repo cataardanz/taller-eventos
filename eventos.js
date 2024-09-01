4fe3871ffa91b56630c3b54f62108842797abc22
cataardanz <cataardanz@gmail.com>
2024-09-01 16:10:22 -0300

    solución click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const div = document.getElementById('div-bttn');
+    const button = div.querySelector('button'); // seleccionamos el button para luego trabajar con addEventL
+
     div.addEventListener('click', () => {
         alert('¡Hola! Soy el div');
+    });
+
+    button.addEventListener('click', (event) => {
+        event.stopPropagation(); // Detiene la propagación del evento click
     });
 });
